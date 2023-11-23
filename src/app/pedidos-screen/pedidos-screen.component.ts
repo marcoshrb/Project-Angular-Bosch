77ee19827024710f7c80718aa4fcffd248ccead4
marcos <marcoshr_baptista@hotmail.com>
2023-11-23 14:43:08 -0300

    screen pedidos

--- src/app/pedidos-screen/pedidos-screen.component.ts
+++ src/app/pedidos-screen/pedidos-screen.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-pedidos-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './pedidos-screen.component.html',
   styleUrl: './pedidos-screen.component.css'
 })
