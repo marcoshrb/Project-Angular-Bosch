09862400e4b333f149fb5b85f240430caedb3816
marcos <marcoshr_baptista@hotmail.com>
2023-11-24 16:50:40 -0300

    totem 24/11

--- src/app/totem-screen/totem-screen.component.ts
+++ src/app/totem-screen/totem-screen.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-totem-screen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './totem-screen.component.html',
   styleUrl: './totem-screen.component.css'
 })
